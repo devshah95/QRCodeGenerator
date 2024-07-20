4253faeb7c8aca7c59bda955e9e3b80265131ac5
devshah95 <devshah95@gmail.com>
2024-07-20 15:18:19 -0400

    Localhost

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -28,4 +28,4 @@ def generate_qr(request: QRRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
