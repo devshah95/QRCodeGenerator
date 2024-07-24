1f44c13dac31b5606e3e6a65aaba4fd77732bc89
devshah95 <devshah95@gmail.com>
2024-07-24 12:06:26 -0400

    Updating TF Code

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,3 +33,55 @@ resource "aws_route_table_association" "route_association" {
   subnet_id = aws_subnet.public.id 
   route_table_id = aws_route_table.public.id
 }
+
+
+resource "aws_security_group" "backend_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 8000
+    to_port = 8000
+    protocol = "tcp"
+    security_groups = [aws_security_group.frontend_sg.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0 
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "frontend_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    security_groups = [aws_security_group.backend_sg.id]
+  }
+
+  egress {
+    from_port = 80 
+    to_port = 80 
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port = 443 
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
