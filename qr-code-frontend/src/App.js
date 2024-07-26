b7d0a064201d22cff0b2d206b7c6a7387cc57b14
devshah95 <devshah95@gmail.com>
2024-07-25 20:10:20 -0400

    Removing var

--- qr-code-frontend/src/App.js
+++ qr-code-frontend/src/App.js
@@ -7,8 +7,7 @@ function App() {
   const [qrCode, setQrCode] = useState(null)
 
   const generateQrCode = () => {
-    const backendUrl =
-      process.env.REACT_APP_BACKEND_URL || "http://localhost:8000"
+    const backendUrl = "http://localhost:8000"
     axios
       .post(
         `${backendUrl}/generate_qr/`,
